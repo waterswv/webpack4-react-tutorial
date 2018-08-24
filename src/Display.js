b7a505d189fb08f3eddd0eb28209080d68410eba
Waterswv <bmierke@gmail.com>
2018-08-24 14:28:12 -0700

    setting up webpack and config with babel & react

---
+++ src/Display.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+const Display = () => <div>Here is my component</div>
+
+export default Display;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Display from './Display';
+
+ReactDOM.render(<Display />, document.getElementById('create-article-form'))
